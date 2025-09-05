70f79bd6ec8f5e11951fd052aef9347f1eb03955
Andres <andresmercado0809@gmail.com>
2025-09-05 15:18:00 -0500

    Update session.py

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -2,7 +2,7 @@ from sqlmodel import SQLModel, Session, create_engine
 from app.core.config import settings
 
 # Crear el motor
-engine = create_engine(settings.DATABASE_URL, echo=True)
+engine = create_engine(settings.DATABASE_URL, echo=False)
 
 
 # Crear para obtener la sesión de la base de datos
