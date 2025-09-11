47a120b4489db40c82d6e5c37d2a4ad3dda46882
Andres <andresmercado0809@gmail.com>
2025-09-11 16:55:55 -0500

    Update config files

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -1,16 +1,28 @@
 import { defineConfig } from "cypress";
+import mochawesome from "cypress-mochawesome-reporter/plugin";
 
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:5173",
-    viewportWidth: 1440,   // ancho
-    viewportHeight: 900,   // alto
+    viewportWidth: 1440,
+    viewportHeight: 900,
+    video: false,                   
+    screenshotOnRunFailure: true,
     env: {
       apiUrl: "http://localhost:8000/api/v1",
     },
+    reporter: "cypress-mochawesome-reporter",
+    reporterOptions: {
+      charts: true,
+      reportPageTitle: "Reporte Cypress",
+      embeddedScreenshots: false,  // no incrusta imágenes en el HTML
+      inlineAssets: true,         // no copia assets dentro del HTML
+    },
     setupNodeEvents(on, config) {
-      // puedes registrar listeners si los necesitas
+      mochawesome(on);
     },
   },
 });
+
+
 
